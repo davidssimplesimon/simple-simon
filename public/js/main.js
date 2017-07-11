f0b3d1741fc3777d025d838ff945ffa5f38a0875
Your Name <you@example.com>
2017-07-11 14:00:48 -0500

    SimpleSimon functions

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,11 @@ function random () {
 	return Math.floor(Math.random() * (4));
 };
 var arches = $('.arch');
-var userPattern = '';
 //round 1
 $('#start').click(function() {
 	var randomArch = random();
-	var pattern = [randomArch];
-	userPattern = '';
+	var pattern = randomArch;
+	var userPattern = '';
 	console.log('first round')
 	console.log('pattern: ' + pattern);
 
@@ -19,27 +18,30 @@ $('#start').click(function() {
 
 	$(arches).click(function(event) {
 		userPattern += event.target.dataset.index;			
+		var	patternArray = pattern.split('');
+		userPatternArray = userPattern.split('');
 		console.log('userPattern: ' + userPattern);
-		if (userPattern == pattern) {
-			console.log('patterns match!');
-			randomArch = random();
-			pattern += randomArch;
-			console.log('Adding to pattern with a new random number...');
-			console.log('pattern: ' + pattern);
-			userPattern = '';
-			patternArray = pattern.split('');
-			patternArray.forEach(function(element, index, array) {					
-				setTimeout(function() {
-						$(arches[element]).css('border', '4px solid black');
-					setTimeout (function() {
-						$(arches[element]).css('border', 'none');						
-					}, 1000);
-				}, 1500 * index);
-			});
+		for (var i = 0; i < patternArray.length; i++) {
+			if (userPatternArray[i] == patternArray[i]) {
+				console.log('patterns match!');
+				randomArch = random();
+				pattern += randomArch;
+				console.log('Adding to pattern with a new random number...');
+				console.log('pattern: ' + pattern);
+				userPattern = '';
+				patternArray.forEach(function(element, index, array) {					
+					setTimeout(function() {
+							$(arches[element]).css('border', '4px solid black');
+						setTimeout (function() {
+							$(arches[element]).css('border', 'none');						
+						}, 1000);
+					}, 1500 * index);
+				});
+			} else {
+				alert('Try Again!')
+			};
 		};
-			
-	});
-	
+	});	
 });
 
 
