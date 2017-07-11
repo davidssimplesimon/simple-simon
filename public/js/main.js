2b949777c2e77b6500a66a77c4f483c16dce9097
David R. Jex <david.jex@me.com>
2017-07-11 14:54:22 -0500

    Functionality completed. Need to add round counter and styling.

--- public/js/main.js
+++ public/js/main.js
@@ -1,48 +1,51 @@
-// (function() {
-function random () {
-	return Math.floor(Math.random() * (4));
-};
-var arches = $('.arch');
-//round 1
-$('#start').click(function() {
-	var randomArch = random();
-	var pattern = randomArch;
-	var userPattern = '';
-	console.log('first round')
-	console.log('pattern: ' + pattern);
-
-	$(arches[randomArch]).css('border', '4px solid black');
-	setTimeout(function() {
-		$(arches[randomArch]).css('border', 'none');
-	}, 1000);
-
-	$(arches).click(function(event) {
-		userPattern += event.target.dataset.index;			
-		var	patternArray = pattern.split('');
-		userPatternArray = userPattern.split('');
-		console.log('userPattern: ' + userPattern);
-		for (var i = 0; i < patternArray.length; i++) {
-			if (userPatternArray[i] == patternArray[i]) {
-				console.log('patterns match!');
-				randomArch = random();
-				pattern += randomArch;
-				console.log('Adding to pattern with a new random number...');
-				console.log('pattern: ' + pattern);
-				userPattern = '';
-				patternArray.forEach(function(element, index, array) {					
-					setTimeout(function() {
-							$(arches[element]).css('border', '4px solid black');
-						setTimeout (function() {
-							$(arches[element]).css('border', 'none');						
-						}, 1000);
-					}, 1500 * index);
-				});
-			} else {
-				alert('Try Again!')
+(function game() {
+	function random () {
+		return Math.floor(Math.random() * (4));
+	};
+	var arches = $('.arch');
+	$('#start').click(function() {
+		var randomArch = random();
+		var patternArray = [randomArch];
+		var userPatternArray = [];
+		console.log('first round')
+		console.log(patternArray);
+	
+		$(arches[randomArch]).css('border', '4px solid black');
+		setTimeout(function() {
+			$(arches[randomArch]).css('border', 'none');
+		}, 1000);
+	
+		$(arches).click(function(event) {
+			userPatternArray.push(parseInt(event.target.dataset.index));			
+			console.log(userPatternArray);
+			console.log(patternArray);
+			for (var i = 0; i < userPatternArray.length; i++) {
+				console.log(userPatternArray[i]);
+				console.log(patternArray[i]);
+				if (userPatternArray[i] != patternArray[i]) {
+					var playAgain = confirm('Game over! Would you like to play again?');
+						if (playAgain) {
+							location.reload();
+						}
+					return;
+				}		
 			};
-		};
-	});	
-});
-
-
-// })();
+			if (userPatternArray.length == patternArray.length) {
+					console.log('patterns match!');
+					randomArch = random();
+					patternArray.push(randomArch);
+					console.log('Adding to pattern with a new random number...');
+					console.log('pattern: ' + patternArray);
+					userPatternArray = [];
+					patternArray.forEach(function(element, index, array) {					
+						setTimeout(function() {
+								$(arches[element]).css('border', '4px solid black');
+							setTimeout (function() {
+								$(arches[element]).css('border', 'none');						
+							}, 1000);
+						}, 1500 * index);
+					});
+				};
+		});	
+	});
+})();
