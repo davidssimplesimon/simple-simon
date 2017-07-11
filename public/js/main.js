a2a397efbf536d9fc77ebfd09791df696c59c950
David R. Jex <david.jex@me.com>
2017-07-11 17:12:44 -0500

    Finished functionality with keypresses, display of pattern, and allowing for finished patter input by user.

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,64 @@
-(function game() {
+(function() {
+	var arches = $('.arch');
+	var randomArch = random();
+	var patternArray = [randomArch];
+	var userPatternArray = [];
 	function random () {
 		return Math.floor(Math.random() * (4));
 	};
-	var arches = $('.arch');
+	function gameOver() {
+		for (var i = 0; i < userPatternArray.length; i++) {
+			if (userPatternArray[i] != patternArray[i]) {
+				var playAgain = confirm('Game over! Would you like to play again?');
+					if (playAgain) {
+						location.reload();
+					};
+				return;
+			};		
+		};
+	};
+	function displayPattern() {
+		if (userPatternArray.length == patternArray.length) {
+			randomArch = random();
+				patternArray.push(randomArch);
+				userPatternArray = [];
+				patternArray.forEach(function(element, index, array) {					
+					setTimeout(function() {
+							$(arches[element]).css('border', '4px solid #ff8f00');
+						setTimeout (function() {
+							$(arches[element]).css('border', 'none');						
+						}, 1000);
+					}, 1500 * index);
+				});
+			$('#round').html(patternArray.length);
+		};
+	};
 	$('#start').click(function() {
-		var randomArch = random();
-		var patternArray = [randomArch];
-		var userPatternArray = [];
-		console.log('first round')
-		console.log(patternArray);
-	
-		$(arches[randomArch]).css('border', '4px solid black');
+		$('#start_button').html('Reset');
+		$('#start_button').click(function() {
+			location.reload();
+		});	
+		$(arches[randomArch]).css('border', '4px solid #ff8f00');
 		setTimeout(function() {
 			$(arches[randomArch]).css('border', 'none');
-		}, 1000);
-	
+		}, 1000);	
 		$(arches).click(function(event) {
 			userPatternArray.push(parseInt(event.target.dataset.index));			
-			console.log(userPatternArray);
-			console.log(patternArray);
-			for (var i = 0; i < userPatternArray.length; i++) {
-				console.log(userPatternArray[i]);
-				console.log(patternArray[i]);
-				if (userPatternArray[i] != patternArray[i]) {
-					var playAgain = confirm('Game over! Would you like to play again?');
-						if (playAgain) {
-							location.reload();
-						}
-					return;
-				}		
-			};
-			if (userPatternArray.length == patternArray.length) {
-					console.log('patterns match!');
-					randomArch = random();
-					patternArray.push(randomArch);
-					console.log('Adding to pattern with a new random number...');
-					console.log('pattern: ' + patternArray);
-					userPatternArray = [];
-					patternArray.forEach(function(element, index, array) {					
-						setTimeout(function() {
-								$(arches[element]).css('border', '4px solid black');
-							setTimeout (function() {
-								$(arches[element]).css('border', 'none');						
-							}, 1000);
-						}, 1500 * index);
-					});
-				};
+			gameOver();
+			displayPattern();
 		});	
+		$(document).keyup(function(event) {
+			if(event.keyCode == 38) {
+				userPatternArray.push(0);
+			} else if(event.keyCode == 40) {
+				userPatternArray.push(3);
+			} else if(event.keyCode == 37) {
+				userPatternArray.push(1);
+			} else if(event.keyCode == 39) {
+				userPatternArray.push(2);
+			}
+			gameOver();
+			displayPattern();
+		})
 	});
 })();
