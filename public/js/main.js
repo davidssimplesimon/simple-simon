061be2e8c8560652bedcceaf1d5579e713c0677a
David R. Jex <david.jex@me.com>
2017-07-11 09:44:14 -0500

    Selects a color and establishes it as a pattern. User is able to mimic pattern but after the first round it will break because the evaluation of the pattern versus the users pattern initiates on the click.

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,37 @@ function random () {
 	return Math.floor(Math.random() * (4));
 };
 var arches = $('.arch');
+var userPattern = '';
 //round 1
 $('#start').click(function() {
 	var randomArch = random();
 	var pattern = [randomArch];
+	console.log('first round')
+	console.log(pattern);
 	$(arches[randomArch]).css('border', '4px solid black');
 	setTimeout(function() {
 		$(arches[randomArch]).css('border', 'none');
 	}, 3000);
+
+	$(arches).click(function(event) {
+		userPattern += event.target.dataset.index;
+		console.log(userPattern);
+		if (userPattern == pattern) {
+			console.log('second round');
+			randomArch = random();
+			pattern += randomArch;
+			userPattern = '';
+			console.log(pattern);
+			console.log(userPattern);
+			// for (var i = 0, i< pattern.length, i++)
+			$(arches[randomArch]).css('border', '4px solid black');
+			setTimeout(function() {
+				$(arches[randomArch]).css('border', 'none');
+			}, 3000);
+		} else {
+			console.log('game over');
+		}
+	});
 	
 })
 
