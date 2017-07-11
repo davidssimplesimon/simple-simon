394a0081135b1658b1ad965eb8157a4a90a64af1
David R. Jex <david.jex@me.com>
2017-07-11 11:03:47 -0500

    Patterns captured. Display of the computer selected pattern needs to be established.

--- public/js/main.js
+++ public/js/main.js
@@ -4,41 +4,37 @@ function random () {
 };
 var arches = $('.arch');
 var userPattern = '';
-pattern = randomString.split("");
 //round 1
 $('#start').click(function() {
 	var randomArch = random();
 	var pattern = [randomArch];
+	userPattern = '';
 	console.log('first round')
-	console.log(pattern);
+	console.log('pattern: ' + pattern);
+
 	$(arches[randomArch]).css('border', '4px solid black');
 	setTimeout(function() {
 		$(arches[randomArch]).css('border', 'none');
 	}, 2000);
 
 	$(arches).click(function(event) {
-		userPattern += event.target.dataset.index;
-		console.log(userPattern);
-				
-			});
-			console.log('second round');
+		userPattern += event.target.dataset.index;			
+		console.log('userPattern: ' + userPattern);
+		if (userPattern == pattern) {
+			console.log('patterns match!');
 			randomArch = random();
 			pattern += randomArch;
+			console.log('Adding to pattern...');
+			console.log('pattern: ' + pattern);
 			userPattern = '';
-
-			console.log(pattern);
-			console.log(userPattern);
-			// for (var i = 0, i< pattern.length, i++)
-		
-				if (userPattern == pattern) {
-			().click(function(event) {
 			$(arches[randomArch]).css('border', '4px solid black');
 			setTimeout(function() {
 				$(arches[randomArch]).css('border', 'none');
 			}, 3000);
+		};
 	});
 	
-};
+});
 
 
 // })();
